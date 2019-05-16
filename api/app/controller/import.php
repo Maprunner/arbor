a235a6ac1681c769f5599d3aca58e43f48abfa0b
Maprunner <simon@maprunner.co.uk>
2019-05-16 22:24:42 +0100

    Add IOF V3 XML import

--- api/app/controller/import.php
+++ api/app/controller/import.php
@@ -48,15 +48,15 @@ private function importCSV($file) {
   $oldClass = '';
   $runners = 0;
   
-  define('FIELD_COUNT', 7);
+  define('FIELD_COUNT', 6);
   define('POS_IDX', 0);
   define('FIRST_NAME_IDX', 1);
   //define('LAST_NAME_IDX', 3);
   define('CLUB_IDX', 2);
   define('CLASS_IDX', 3);
-  define('TIME_IDX', 5);
-  define('STATUS_IDX', 6);
-  define('LENGTH_IDX', 4);
+  define('TIME_IDX', 4);
+  define('STATUS_IDX', 5);
+  define('LENGTH_IDX', 0);
 
   foreach ($lines as $line) {
     $r = explode(",", trim($line));
@@ -79,7 +79,8 @@ private function importCSV($file) {
       $classTable->reset();
       $classTable->RaceID = $race->RaceID;
       $classTable->Class = $r[CLASS_IDX];
-      $classTable->Length = str_replace('km', '', $r[LENGTH_IDX]);
+      //$classTable->Length = str_replace('km', '', $r[LENGTH_IDX]);
+      $classTable->Length = 0;
       $classTable->Runners = 0;
       $classTable->save();
       //echo "Save ".$r[CLASS_IDX]."<br>";
@@ -121,12 +122,19 @@ private function importCSV($file) {
 }
 
 private function importXML($file) {
-  // extracts info from XML file exported from Winsplits
+  // extracts info from v2.0.3 XML file exported from Winsplits
   $xml = simplexml_load_file($file);
   if ($xml === false) {
     echo "Invalid XML file<br>";
     return;
   }
+  // <IOFVersion> only present for V2 XML files
+  ($xml->IOFVersion) ? 
+    $this->importXMLV2($xml):
+    $this->importXMLV3($xml);
+}
+
+private function importXMLV2($xml) {
   $result = new DB\SQL\Mapper($this->db,'result');
   $classTable = new DB\SQL\Mapper($this->db,'class');
   $race = new DB\SQL\Mapper($this->db,'race');
@@ -143,7 +151,7 @@ private function importXML($file) {
       $classTable->Length = 0;
       $classTable->Runners = 0;
       $classTable->save();
-      //echo "Save ".$class->ClassShortName."<br>";
+      echo "Save ".$class->ClassShortName."<br>";
       $newClass = false;
    	  $classRunners = 0;
     }
@@ -173,7 +181,7 @@ private function importXML($file) {
       $result->ClassID = $classTable->ClassID;
   		$result->Time = $personresult->Result->Time->Clock;
   		$result->Position = $personresult->Result->ResultPosition;
-  		$result->Status = $this->getStatus($personresult->Result->CompetitorStatus->attributes());
+  		$result->Status = $this->getV2Status($personresult->Result->CompetitorStatus->attributes());
   		if ($result->Status != 'OK') $result->Position = 999;
       $result->save();
       //echo 'Save '.$result->Name.'<br>';
@@ -197,12 +205,92 @@ private function importXML($file) {
   $race->Runners = $runners;
   $race->save();
 }
+ 
+private function importXMLV3($xml) {
+  $result = new DB\SQL\Mapper($this->db,'result');
+  $classTable = new DB\SQL\Mapper($this->db,'class');
+  $race = new DB\SQL\Mapper($this->db,'race');
+  $race->load(array('RaceID = :id',':id'=>$this->id));
+  $classes = 0;
   
-private function isUTF8($string) {
-    return (utf8_encode(utf8_decode($string)) == $string);
+  $newClass = true;
+  $runners = 0;
+  foreach ($xml->ClassResult as $class) {
+    if ($newClass) {
+      $classTable->reset();
+      $classTable->RaceID = $race->RaceID;
+      $classTable->Class = $class->Class->Name;
+      $classTable->Length = 0;
+      $classTable->Runners = 0;
+      $classTable->save();
+      // echo "Save ".$class->Class->Name."<br>";
+      $newClass = false;
+   	  $classRunners = 0;
+    }
+  	foreach ($class->PersonResult as $personresult) {
+      $result->reset();
+      // ResultID is set automatically when result is saved
+      $result->RaceID = $race->RaceID;
+  		$result->BOFID = $personresult->Person->Id;
+      $result->Year = $race->Year;
+      $result->Event = $this->getEventAbbr($race->Event);
+      $fullname = $personresult->Person->Name;
+      $runner = trim($fullname->Given)." ".trim($fullname->Family);
+  		// removes commas to avoid problems with CSV import in Excel: not a problem any longer but anyway...
+      $result->Name = str_replace(',', ' ', $runner);
+      // horrid mess to sort out character mangling through various text files
+      // make sure XML file decalres UTF-8: save via Notepad if necessary
+      if (!$this->isUTF8($result->Name)) { 
+        $result->Name = iconv("ISO-8859-1", "UTF-8//TRANSLIT", $result->Name);
+      }
+  		$club = $personresult->Organisation->Name;
+      $result->Club = str_replace(',', ' ', $club);
+      if (!$this->isUTF8($result->Club)) { 
+        $result->Club = iconv("ISO-8859-1", "UTF-8//TRANSLIT", $result->Club);
+      }
+      $result->Card = 0;
+      $result->Class = $class->Class->Name;
+      $result->ClassID = $classTable->ClassID;
+      $secs = $personresult->Result->Time;
+  		$result->Time = $this->getTimeFromSeconds($secs);
+      $result->Position = $personresult->Result->Position;
+      $result->Status = $this->getV3Status($personresult->Result->Status);
+      // don't save Did Not Start records
+      if ($result->Status == 'DNS') {
+        continue;
+      }
+  		if ($result->Status != 'OK') $result->Position = 999;
+      $result->save();
+      // echo 'Save '.$result->Name.'<br>';
+			$classRunners++;
+    }
+    // update class details
+    $classTable->Length = $personresult->Result->Length;
+    $classTable->Runners = $classRunners;
+    $classTable->save();
+    echo $classRunners."  ".$class->Class->Name." at ".date('H:i:s')."<br>";
+    $newClass = true;
+    $classes++;
+    $runners = $runners + $classRunners;
+    $classRunners = 0;
+
+  }
+  
+  // update race info
+  $race->Classes = $classes;
+  $race->Runners = $runners;
+  $race->save();
 }
 
-private function getStatus($attrs) {
+private function isUTF8($string) {
+   return (utf8_encode(utf8_decode($string)) == $string);
+}
+
+private function getTimeFromSeconds($secs) {
+  return sprintf( "%02.2d:%02.2d", floor( $secs / 60 ), $secs % 60 );;
+}
+
+private function getV2Status($attrs) {
   $status = (string) $attrs["value"];
 	if ($status == 'MisPunch') $status = 'mp';
 	if ($status == 'DidNotStart') $status = 'dns';
@@ -210,7 +298,24 @@ private function getStatus($attrs) {
   if ($status == 'DidNotFinish') $status = 'dnf';
   return $status;
 }
-  
+
+private function getV3Status($status) {
+  switch ($status) {
+    case 'Disqualified':
+      return 'DSQ';
+    case 'MisPunch':
+      return 'DSQ';
+    case 'OverTime':
+      return 'OT';
+    case 'DidNotFinish':
+      return 'DNF';
+    case 'DidNotStart':
+      return 'DNS';
+    default:
+      return $status;
+  }
+}
+
 private function getEventAbbr($event) {
   switch ($event) {
     case "British Long":
